6efae6e5e5aa393ac8e31db041050c53d5e19f0f
Tracy Smith <tracysuzanne321@gmail.com>
2022-01-30 22:23:23 +0000

    functions-components

--- src/App.js
+++ src/App.js
@@ -1,31 +1,50 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
+import SingleCard from './components/SingleCard';
+
 const cardImg = [
-	{ src: '/images/arms.png' },
-	{ src: '/images/potion.png' },
-	{ src: '/images/scroll.png' },
-	{ src: '/images/sheild.png' },
-	{ src: '/images/singleSword.png' },
-	{ src: '/images/sword.png' },
+	{ src: '/img/arms.png' },
+	{ src: '/img/potion.png' },
+	{ src: '/img/scroll.png' },
+	{ src: '/img/sheild.png' },
+	{ src: '/img/singleSword.png' },
+	{ src: '/img/sword.png' },
 ];
 
 const App = () => {
-	const [card, setCard] = useState([]);
+	const [cards, setCards] = useState([]);
 	const [turns, setTurns] = useState(0);
+	const [choice1, setChoice1] = useState(null);
+	const [choice2, setChoice2] = useState(null);
 
 	const shuffleCards = () => {
 		const shuffledCards = [...cardImg, ...cardImg]
 			.sort(() => Math.random() - 0.5)
 			.map((card) => ({ ...card, id: Math.random() }));
-		setCard(shuffledCards);
+		setCards(shuffledCards);
 		setTurns(0);
 	};
+	const handleChoice = (card) => {
+		choice1 ? setChoice2(card) : setChoice1(card);
+	};
 
-	console.log(card, turns);
+	const resetTurn = () => {
+		setChoice1(null);
+		setChoice2(null);
+		setTurns((prevTurns) => prevTurns + 1);
+	};
+
+	useEffect(() => {}, [choice1, choice2]);
+
 	return (
 		<div className="App">
 			<h1>Memory Game</h1>
 			<button onClick={shuffleCards}>Start Game</button>
+			<div className="card-grid">
+				{cards.map((card) => (
+					<SingleCard card={card} handleChoice={handleChoice} />
+				))}
+			</div>
 		</div>
 	);
 };
---
+++ src/components/SingleCard.js
@@ -0,0 +1,22 @@
+import './singleCard.css';
+
+const SingleCard = ({ card, handleChoice }) => {
+	const handleClick = () => {
+		handleChoice(card);
+	};
+	return (
+		<div className="card">
+			<div>
+				<img className="front" src={card.src} alt="card front" />
+				<img
+					className="back"
+					src="/img/cover.png"
+					alt="card back"
+					onClick={handleClick}
+				/>
+			</div>
+		</div>
+	);
+};
+
+export default SingleCard;
