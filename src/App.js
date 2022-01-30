f4b9dbe274558f979a79031579809239d1748714
Tracy Smith <tracysuzanne321@gmail.com>
2022-01-30 21:15:25 +0000

    shuffle function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import { useState } from 'react';
 import './App.css';
+const cardImg = [
+	{ src: '/images/arms.png' },
+	{ src: '/images/potion.png' },
+	{ src: '/images/scroll.png' },
+	{ src: '/images/sheild.png' },
+	{ src: '/images/singleSword.png' },
+	{ src: '/images/sword.png' },
+];
 
 const App = () => {
-	return <div className="App"></div>;
+	const [card, setCard] = useState([]);
+	const [turns, setTurns] = useState(0);
+
+	const shuffleCards = () => {
+		const shuffledCards = [...cardImg, ...cardImg]
+			.sort(() => Math.random() - 0.5)
+			.map((card) => ({ ...card, id: Math.random() }));
+		setCard(shuffledCards);
+		setTurns(0);
+	};
+
+	console.log(card, turns);
+	return (
+		<div className="App">
+			<h1>Memory Game</h1>
+			<button onClick={shuffleCards}>Start Game</button>
+		</div>
+	);
 };
 
 export default App;
